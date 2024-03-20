3d0b9a371f8278027e6db9bb9f5ed7794afba023
Paweł Brzozowski <pawel@brzoza.net>
2024-03-20 12:03:05 +0100

    POST joi validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
 
 const {
@@ -8,6 +9,12 @@ const {
   removeContact,
   updateContact,
 } = require("../../models/contacts.js");
+
+const userPattern = Joi.object({
+  name: Joi.string().min(5).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(9).required(),
+});
 
 router.get("/", async (request, response, next) => {
   try {
@@ -38,18 +45,38 @@ router.get("/:contactId", async (request, response, next) => {
   }
 });
 
+// NOTE: basic validation
+// router.post("/", async (request, response, next) => {
+//   try {
+//     const body = request.body;
+//     const addedContact = await addContact(body);
+
+//     if (!body.name || !body.email || !body.phone) {
+//       return response
+//         .status(400)
+//         .json({ message: "missing required field(s)" });
+//     } else {
+//       return response.json(addedContact);
+//     }
+//   } catch (error) {
+//     console.error("Error during add contact: ", error);
+//     next(error);
+//   }
+// });
+
+// NOTE: joi validation
 router.post("/", async (request, response, next) => {
   try {
     const body = request.body;
-    const addedContact = await addContact(body);
+    const { error } = userPattern.validate(body);
 
-    if (!body.name || !body.email || !body.phone) {
-      return response
-        .status(400)
-        .json({ message: "missing required field(s)" });
-    } else {
-      return response.json(addedContact);
+    if (error) {
+      const errorMessage = error.details[0].message;
+      return response.status(400).json({ message: `${errorMessage}` });
     }
+
+    const addedContact = await addContact(body);
+    return response.json(addedContact);
   } catch (error) {
     console.error("Error during add contact: ", error);
     next(error);
