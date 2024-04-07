ce0c9b0fe800d9f174927a36105d613085b68a3c
Paweł Brzozowski <pawel@brzoza.net>
2024-04-07 15:13:21 +0200

    Fix sendVerificationEmail fn

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -3,8 +3,8 @@ sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendVerificationEmail = async (userEmail, next, verificationToken) => {
   const verificationMessage = {
-    // to: `${userEmail}`,
-    to: `pawel@brzoza.net`,
+    to: `${userEmail}`,
+    // to: `pawel@brzoza.net`, // NOTE: uncomment it for tests
     from: `pawel@brzoza.net`,
     subject: `Confirm your email address`,
     text: `Click on the link to confirm your account: http://localhost:3000/api/users/verify/${verificationToken}`,
